0736b3fc98c83e8305c2b791ddd1e7f38eb55634
Tim Donoghue <tmd112593@gmail.com>
2020-02-22 18:02:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,8 @@
 def key_for_min_value(hash)
-  lowest_key == nil
-  lowest_value == nil 
+  lowest_key = nil
+  lowest_value = nil 
   hash.each do |key, value|
-      if lowest_value == nil || value < lowest.value 
+      if lowest_value == nil || value < lowest_value 
         lowest_value == value
         lowest_key == key
       end
